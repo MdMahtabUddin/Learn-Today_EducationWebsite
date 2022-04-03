440064d821bc2d6c4afa8a12ed5b60ba98516746
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2022-04-04 01:26:19 +0600

    updated hero section

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Card, Carousel, Col, Row } from 'react-bootstrap';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import '../Home/Home.css';
 import CourseInfo from '../CourseInfo/CourseInfo';
-
+import image from './JS.gif'
 
 const Home = () => {
   const [course, setCourse] = useState([]);
@@ -23,10 +23,43 @@ const Home = () => {
   return (
 
     <div>
-      <div className="banner-container text-start">
-        <h1 className="ms-5">Never Stop Learning </h1>
-        <button className="btn btn-lg-outline btn-info fs-5 fw-bolder ms-5 text-primary">Learn More</button>
-      </div>
+      <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={image}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      {/* <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
       <h3 className="mb-4">Popular Course</h3>
       <Row xs={1} md={2} lg={4} sm={1} className="g-4">
         {
